ef536e1b49bfa49b167489747d33de1a404e077e
mryusuf <muhamadyusufindra@gmail.com>
2020-07-12 10:50:21 +0700

    Initial Commit

---
+++ Game In-Depth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game In-Depth
+//
+//  Created by Indra Permana on 12/07/20.
+//  Copyright © 2020 IndraPP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
